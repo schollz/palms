0af6de3a67a44f58afaa64a15443c5fbe1cd93f5
Zack Scholl <zack.scholl@gmail.com>
2022-05-02 14:51:03 -0700

    add zverb

---
+++ ZVerb.cpp
@@ -0,0 +1,37 @@
+#include "ZVerb.h"
+#include <math.h>
+#include <stdexcept>
+
+float randfloat(float a, float b) {
+    return ((b - a) * ((float)rand() / RAND_MAX)) + a;
+}
+
+ZVerb::ZVerb() {}
+
+ZVerb::ZVerb(float wet, float roomsize, float samplingRate) {
+    int ret;
+    if ((ret = setup(wet, roomsize, samplingRate)))
+        throw std::runtime_error("feedback comb: decay is less then delay");
+}
+
+int ZVerb::setup(float wet, float roomsize, float samplingRate) {
+    if (wet < 0) {
+        return -1;
+    }
+    wet_ = wet;
+    roomsize_ = roomsize;
+    samplingRate_ = samplingRate;
+    for (unsigned int i = 0; i < ZVERB_COMB; i++) {
+        comb[i] =
+            FeedbackComb(randfloat(0.01, 0.99), randfloat(0.1, 6) * roomsize_,
+                         randfloat(0.25, 0.75), samplingRate_);
+    }
+
+    for (unsigned int i = 0; i < ZVERB_AP; i++) {
+        allpass[i] = Allpass(randfloat(0.01, 0.99),
+                             randfloat(0.1, 6) * roomsize_, 1, samplingRate_);
+    }
+    return 0;
+}
+
+float ZVerb::process(float input) { return 0; }
---
+++ ZVerb.h
@@ -0,0 +1,21 @@
+#include "Allpass.h"
+#include "FeedbackComb.h"
+#include <vector>
+
+#pragma once
+
+#define ZVERB_COMB 10
+#define ZVERB_AP 4
+
+class ZVerb {
+  public:
+    ZVerb();
+    ZVerb(float wet, float roomsize, float samplingRate);
+    int setup(float wet, float roomsize, float samplingRate);
+    float process(float input);
+
+  private:
+    float wet_, roomsize_, samplingRate_;
+    FeedbackComb comb[ZVERB_COMB];
+    Allpass allpass[ZVERB_AP];
+};
